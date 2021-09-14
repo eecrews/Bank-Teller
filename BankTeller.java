601f54997e10315340daf76bf9b32e9c0d46cee6
eecrews <61025723+eecrews@users.noreply.github.com>
2021-09-14 12:35:45 -0500

    Removed personal information

--- BankTeller.java
+++ BankTeller.java
@@ -3,23 +3,7 @@
 // Title:           P04 Exceptional Bank Teller
 // Files:           BankAccount.java, BankAccountTester.java, BankTeller.java,
 //                  BankTellerTester.java
-// Course:          CS 300, fall, 2019
-//
-// Author:          Erin Crews
-// Email:           eecrews@wisc.edu
-// Lecturer's Name: Gary Dahl
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name:    N/A
-// Partner Email:   N/A
-// Partner Lecturer's Name: N/A
-// 
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-//   ___ Write-up states that pair programming is allowed for this assignment.
-//   ___ We have both read and understand the course Pair Programming Policy.
-//   ___ We have registered our team prior to the team registration deadline.
-//
+// Course:          CS 300
 ///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
 //
 // Students who get help from sources other than their partner must fully 
